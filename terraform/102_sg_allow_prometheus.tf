4d855ead69bdd368928ba4b5135c8ffd1a660d74
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-11 16:06:21 +0100

    Add 102_sg_allow_prometheus.tf

--- terraform/102_sg_allow_prometheus.tf
+++ terraform/102_sg_allow_prometheus.tf
@@ -5,11 +5,10 @@ resource "aws_security_group" "allow_prometheus" {
 
   ingress {
     from_port = 9100
-    to_port = 9100
+    to_port = 9110
     protocol = "tcp"
     cidr_blocks = ["${aws_subnet.monitoring.cidr_block}"]
   }
-
 
   tags {
     Name = "${var.project_name} - allow prometheus"
