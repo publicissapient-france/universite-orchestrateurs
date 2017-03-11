fb618ad90dfde02d10a77408c8a64e408b7d5f54
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-11 16:06:05 +0100

    Remove icmp from allow_ssh

--- terraform/100_sg_allow_ssh.tf
+++ terraform/100_sg_allow_ssh.tf
@@ -9,19 +9,6 @@ resource "aws_security_group" "allow_ssh" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = [
-      "0.0.0.0/0"]
-  }
-  ingress {
-    protocol = "icmp"
-    from_port = 30
-    # traceroute
-    to_port = 0
-  }
 
   tags {
     Name = "${var.project_name} - public sg"
