cad3793ba3ff0bc6312815a9f2d7c37b7fb08b7f
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-26 13:26:47 +0200

    Update Jenkinsfile.k8s.groovy

--- applications/click-count/Jenkinsfile.k8s.groovy
+++ applications/click-count/Jenkinsfile.k8s.groovy
@@ -1,47 +1,48 @@
-podTemplate(label: 'mypod', containers: [
-		containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
+podTemplate(label: 'mavenPod', inheritFrom: 'mypod' , containers: [
+        containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
 ]) {
 
-	node('mypod') {
-		checkout scm
+    node('mavenPod') {
+        checkout scm
 
-		def version
-        container('maven') {
-            stage('Preparation') {
-                sh 'git rev-parse HEAD > GIT_COMMIT'
-                version = readFile('GIT_COMMIT').take(6)
-            }
+        def version
+        stage('Preparation') {
+            sh 'git rev-parse HEAD > GIT_COMMIT'
+            version = readFile('GIT_COMMIT').take(6)
+        }
 
-            stage('Build') {
+        stage('Build') {
+            container('maven') {
                 dir('applications/click-count') {
                     sh 'mvn clean package'
                 }
             }
+        }
 
-            stage('Results') {
-                dir('applications/click-count') {
-                    archive 'target/clickCount.war'
-                }
-            }
-
-            stage('Build image') {
-                dir('applications/click-count') {
-                    sh "docker build -t registry.mesos.uo.techx.fr/xebiafrance/click-count:${version} ."
-                }
-            }
-
-            stage('Push image') {
-                dir('applications/click-count') {
-                    sh "docker push registry.mesos.uo.techx.fr/xebiafrance/click-count:${version}"
-                }
-            }
-
-            stage('Deploy on Staging') {
-                dir('applications/click-count') {
-                    sh "sed -i 's#{{.VERSION}}#${version}#' marathon.json"
-                    sh "curl -X PUT -H 'Content-type: application/json' http://mesos-master1.private:8080/v2/groups -d@marathon.json"
-                }
+        stage('Results') {
+            dir('applications/click-count') {
+                archive 'target/clickCount.war'
             }
         }
-	}
+
+        stage('Build image') {
+            dir('applications/click-count') {
+                sh "docker build -t registry.mesos.uo.techx.fr/xebiafrance/click-count:${version} ."
+            }
+        }
+
+        stage('Push image') {
+            dir('applications/click-count') {
+                sh "docker push registry.mesos.uo.techx.fr/xebiafrance/click-count:${version}"
+            }
+        }
+
+        stage('Deploy on Staging') {
+            dir('applications/click-count') {
+                sh "sed -i 's#{{.VERSION}}#${version}#' marathon.json"
+                sh "curl -X PUT -H 'Content-type: application/json' http://mesos-master1.private:8080/v2/groups -d@marathon.json"
+            }
+        }
+
+    }
 }
