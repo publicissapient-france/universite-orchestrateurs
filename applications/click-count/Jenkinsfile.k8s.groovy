3254ba8bbc4b181b476f160ffa6462d44438ee6b
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-26 11:37:57 +0200

    Add jenkins file for k8s

---
+++ applications/click-count/Jenkinsfile.k8s.groovy
@@ -0,0 +1,41 @@
+node {
+	checkout scm
+
+	def version
+
+	stage('Preparation') {
+		sh 'git rev-parse HEAD > GIT_COMMIT'
+		version = readFile('GIT_COMMIT').take(6)
+	}
+
+	stage('Build') {
+		dir('applications/click-count') {
+			sh 'mvn clean package'
+		}
+	}
+
+	stage('Results') {
+		dir('applications/click-count') {
+			archive 'target/clickCount.war'
+		}
+	}
+
+	stage('Build image') {
+		dir('applications/click-count') {
+			sh "docker build -t registry.mesos.uo.techx.fr/xebiafrance/click-count:${version} ."
+		}
+	}
+
+	stage('Push image') {
+		dir('applications/click-count') {
+			sh "docker push registry.mesos.uo.techx.fr/xebiafrance/click-count:${version}"
+		}
+	}
+
+	stage('Deploy on Staging') {
+		dir('applications/click-count') {
+			sh "sed -i 's#{{.VERSION}}#${version}#' marathon.json"
+			sh "curl -X PUT -H 'Content-type: application/json' http://mesos-master1.private:8080/v2/groups -d@marathon.json"
+		}
+	}
+}
