a34d9f63d6bede8dac586c0947b016980c568576
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-31 15:11:04 +0200

    Update Jenkinsfile.k8s.groovy

--- applications/click-count/src/main/java/fr/xebia/clickcount/repository/ClickRepository.java
+++ applications/click-count/src/main/java/fr/xebia/clickcount/repository/ClickRepository.java
@@ -27,9 +27,9 @@ public class ClickRepository {
     @Inject
     public ClickRepository(Configuration configuration) {
         Config config = new Config();
-        String redisAddr = String.format("%s:%d", configuration.redisHost, configuration.redisPort)
+        String redisAddr = String.format("%s:%d", configuration.redisHost, configuration.redisPort);
         config.useSingleServer().setAddress(redisAddr);
-        log.info("Using redis: " + redisAddr)
+        log.info("Using redis: " + redisAddr);
         redisson = Redisson.create(config);
         redisClient = new RedisClient(new NioEventLoopGroup(), NioSocketChannel.class, configuration.redisHost, configuration.redisPort, configuration.redisConnectionTimeout);
     }
