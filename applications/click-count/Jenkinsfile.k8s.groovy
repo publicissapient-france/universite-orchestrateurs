d475a7affb56816560dfe457777029d16c8cd1e8
Thomas Auffredou <tauffredou@xebia.fr>
2017-03-31 10:57:51 +0200

    Revert "Update Jenkinsfile.k8s.groovy"
    This reverts commit 9db90b974e04b8821ca8182b49c8afd2c9ae7e34.

--- applications/click-count/Jenkinsfile.k8s.groovy
+++ applications/click-count/Jenkinsfile.k8s.groovy
@@ -12,7 +12,6 @@ podTemplate(label: 'mavenPod', inheritFrom: 'mypod', containers: [
         container('ssh') {
 //            checkout scm
             stage('Preparation') {
-                sh "git rev-parse --short HEAD > GIT_COMMIT"
                 version = readFile('GIT_COMMIT').take(6)
             }
         }
