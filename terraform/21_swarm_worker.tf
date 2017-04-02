e9293bb771eb1ad087e7b28ec8778c4e863bd748
Alexis Horgix Chotard <alexis.horgix.chotard@gmail.com>
2017-04-02 17:15:59 +0200

    terraform: add rexray iam role to swarm workers

--- terraform/21_swarm_worker.tf
+++ terraform/21_swarm_worker.tf
@@ -14,6 +14,8 @@ resource "aws_instance" "swarm-worker" {
   associate_public_ip_address = true
   subnet_id                   = "${aws_subnet.swarm-worker.id}"
 
+  iam_instance_profile = "${aws_iam_instance_profile.rexray-worker.id}"
+
   vpc_security_group_ids = [
     "${aws_security_group.allow_ssh.id}",
     "${aws_security_group.allow_prometheus.id}",
