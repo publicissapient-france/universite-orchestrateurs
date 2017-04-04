1a1ae8dff928fd4217b57270eb5806857527a7cc
Thomas Auffredou <tauffredou@xebia.fr>
2017-04-05 00:03:43 +0200

    Allow port range

--- terraform/106_sg_public_app.tf
+++ terraform/106_sg_public_app.tf
@@ -18,6 +18,16 @@ resource "aws_security_group" "allow_public_app" {
   }
 
   ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
     from_port = 6443
     to_port   = 6443
     protocol  = "tcp"
@@ -40,6 +50,17 @@ resource "aws_security_group" "allow_public_app" {
   ingress {
     from_port = 8000
     to_port   = 10000
+    protocol  = "tcp"
+
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 30000
+    to_port   = 40000
     protocol  = "tcp"
 
 
